cfb0e8ac57994951e00389337bbc11369641badc
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2026-05-26 22:27:05 +0000

    [Auto] update Casks/dotnet-sdk9-0-300.rb from 9.0.306 to 9.0.314

--- Casks/dotnet-sdk9-0-300.rb
+++ Casks/dotnet-sdk9-0-300.rb
@@ -1,10 +1,10 @@
 cask "dotnet-sdk9-0-300" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.0.306,9.0.10"
+  version "9.0.314,9.0.16"
 
-  sha256_x64 = "5f50fe83c5b3ca85c48276c1c35402be7c83916b0f0f5fef3fa2c893539746df"
-  sha256_arm64 = "7bb202952ee7f64873c2f195fc2eea5a0191c443b1644f382c44c9f3b2bcd3e7"
+  sha256_x64 = "cf00dac87379a9b55dc3b466b6ac352e3f6283b10a35c9501a5b165f85273723"
+  sha256_arm64 = "0aa9b030b78a08a990f20e7d5faf8a9243a4211b2e9a4fc50103fd9a27bbe21d"
   url_x64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
   url_arm64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
